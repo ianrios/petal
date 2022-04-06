913150d5c7e8601c0136c75283928f7526df930a
Ian Rios <ianrios@Ians-MacBook-Pro.local>
2022-04-06 15:23:47 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Plant from "./Plant";
+import styled from "styled-components";
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Wrapper className="App">
+      <h1 style={{ textAlign: 'center' }}>Petal</h1>
+      <h3 style={{ textAlign: 'center' }}>a virtual instrument by ian rios</h3>
+      <Canvas>
+
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Plant />
+        </Suspense>
+      </Canvas>
+
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 20px;
+  position: relative;
+  background: #ffffff;
+  
+  canvas {
+    height: 100vh;
+  }
+`
 
 export default App;
